ccb47851bcbb09746e8512f011011afa8bbb71e7
vanzetti777 <t.bants@yandex.ru>
2025-09-18 23:22:39 +0300

    kodi

---
+++ коды/src/06.py
@@ -0,0 +1,12 @@
+n=int(input('in_1: '))
+k=1
+och=0
+zao=0
+for i in range(n):
+    k+=1
+    name1, name2,voz , obu = (input(f'in{k}: ')).split()
+    if obu=="True":
+        och+=1
+    else:
+        zao+=1
+print(f'out: {och},{zao}')
