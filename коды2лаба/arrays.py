5b847d148b1e9a0710279423edde6d2a9c473bc3
vanzetti777 <t.bants@yandex.ru>
2025-09-26 14:50:09 +0300

    1

--- коды2лаба/arrays.py
+++ коды2лаба/arrays.py
@@ -1,45 +1,45 @@
-a=[3,-1,5,5,0]
-b=[42]
-c=[-5, -2, -9]
-d=[]
-e=[1.5, 2, 2.0, -3.1]
-def f(b):
-    a=tuple(b)
-    s1=[]
-    for i in a:
-        s1.append(i)
-    if len(s1)==0:
-        print('ValueError')
-    else:
-        s2=(min(s1),max(s1))
-        print(s2)
-print(f(a))
-print(f(b))
-print(f(c))
-print(f(d))
-print(f(e))
+# a=[3,-1,5,5,0]
+# b=[42]
+# c=[-5, -2, -9]
+# d=[]
+# e=[1.5, 2, 2.0, -3.1]
+# def f(b):
+#     a=tuple(b)
+#     s1=[]
+#     for i in a:
+#         s1.append(i)
+#     if len(s1)==0:
+#         print('ValueError')
+#     else:
+#         s2=(min(s1),max(s1))
+#         print(s2)
+# print(f(a))
+# print(f(b))
+# print(f(c))
+# print(f(d))
+# print(f(e))
 
 
 
-a=[3, 1, 2, 1, 3] 
-b=[]
-c=[-1, -1, 0, 2, 2]
-d=[1.0, 1, 2.5, 2.5, 0]
-def f(a):
-    g=[]
-    h=[]
-    for i in a:
-        if type(i)==int:
-            h.append(i)
-        else:
-            g.append(i)
-    res1=[x for x in g if int(x) in h]
-    res2=[x for x in h if float(x) not in g]
-    print(sorted(set(res1+res2+g)))
-print(f(a))
-print(f(b))
-print(f(c))
-print(f(d))
+# a=[3, 1, 2, 1, 3] 
+# b=[]
+# c=[-1, -1, 0, 2, 2]
+# d=[1.0, 1, 2.5, 2.5, 0]
+# def f(a):
+#     g=[]
+#     h=[]
+#     for i in a:
+#         if type(i)==int:
+#             h.append(i)
+#         else:
+#             g.append(i)
+#     res1=[x for x in g if int(x) in h]
+#     res2=[x for x in h if float(x) not in g]
+#     print(sorted(set(res1+res2+g)))
+# print(f(a))
+# print(f(b))
+# print(f(c))
+# print(f(d))
 
 
 
